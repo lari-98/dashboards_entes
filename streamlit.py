c3f34b8edf73f3765eed14d2b0e27ee6aa9efbfd
Larissa <larissapg.bsb@gmail.com>
2022-10-07 14:38:42 -0300

    barplot implementation

--- streamlit.py
+++ streamlit.py
@@ -6,8 +6,17 @@ from SC_dataframe import sc
 
 st.write("Informações de Santa Catarina")
 
+# Utilizando metric
 st.metric(label="Quantidade de Entes em SC", value=len(uf_selecionadas), delta="Santa Catarina")
 st.metric(label="Quantidade de Entes de SC com população maior que 10 mil habitantes", value=len(uf_maiores), delta="Santa Catarina")
 st.metric(label="Quantidade de Entes de SC com população menor que 5 mil habitantes", value=len(filter_5(uf_selecionadas)), delta="Santa Catarina")
+
+# Utilizando barplot
+
+sc_bp = pd.DataFrame(
+    sc,
+    columns=["populacao", "ente"])
+
+st.bar_chart(data=sc_bp, y="População", x="Nome do Ente", use_container_width=True)
 
 sc
